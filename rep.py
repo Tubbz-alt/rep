00354f3789dcb55aa5440e43e11e2cb2a9b1dd9a
hyperbeamf <hbeam@pop-os.localdomain>
2020-03-22 17:28:33 -0400

    Replicates to the source directory of the hard drive

--- rep.py
+++ rep.py
@@ -11,15 +11,15 @@ maccmd   = "open message.txt"
 if platform == "linux" or platform == "linux2":
     os.system(linuxcmd)
     os.mkdir('clone')
-    os.system("cp message.txt clone/")
-    os.system("cp " + name + " clone/")
+    os.system("cp message.txt /clone/")
+    os.system("cp " + name + " /clone/")
 elif platform == "darwin":
     os.system(maccmd)
     os.mkdir('clone')
-    os.system("cp message.txt clone/")
-    os.system("cp " + name + " clone/")
+    os.system("cp message.txt /clone/")
+    os.system("cp " + name + " /clone/")
 elif platform == "win32":
     os.system(wincmd)
     os.mkdir('clone')
-    os.system("copy message.txt clone")
-    os.system("copy " + name + " clone")
+    os.system("copy message.txt C:/clone")
+    os.system("copy " + name + " C:/clone")
